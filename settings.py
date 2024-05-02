66df8f9166d97f6979ee521b849052725520ac86
pattckc <71180317+pattckc@users.noreply.github.com>
2024-05-02 21:43:30 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -49,12 +49,12 @@
 # }
 
 env_kwargs = {
-    "n_pursuers": 4,
+    "n_pursuers": 6,
     "n_evaders": 6,
     "n_poisons": 8,
     "n_coop": 2,
     "n_sensors": 16,
-    "sensor_range": 0.2,
+    "sensor_range": 0.04,
     "radius": 0.015,
     "obstacle_radius": 0.055,
     "n_obstacles": 1,
@@ -70,6 +70,12 @@ env_kwargs = {
     "speed_features": True,
     "max_cycles": 1000
 }
+
+# waterworld_v4.env(n_pursuers=5, n_evaders=5, n_poisons=10, n_coop=2, n_sensors=20,
+# sensor_range=0.2,radius=0.015, obstacle_radius=0.2, n_obstacles=1,
+# obstacle_coord=[(0.5, 0.5)], pursuer_max_accel=0.01, evader_speed=0.01,
+# poison_speed=0.01, poison_reward=-1.0, food_reward=10.0, encounter_reward=0.01,
+# thrust_penalty=-0.5, local_ratio=1.0, speed_features=True, max_cycles=500)
 
 
 # Hyperparameter space for PPO
